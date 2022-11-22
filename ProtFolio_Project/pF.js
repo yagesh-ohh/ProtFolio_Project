0589fe84c8a12e11b852d51f43c1c844e66cabe5
yagesh143 <102805114+yagesh143@users.noreply.github.com>
2022-11-22 15:04:55 +0530

    Add files via upload

---
+++ ProtFolio_Project/pF.js
@@ -0,0 +1,29 @@
+// Show Menu
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav_toggle", "nav_menu");
+
+// Active & Remove Active
+
+var navLink = document.querySelectorAll(".nav_link");
+navLink.forEach((n) => n.classList.remove("active"));
+
+function linkAction() {
+  this.classList.add("active");
+
+  const navMenu = document.getElementById("nav_menu");
+  navMenu.classList.remove("show");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// -----------------------------------------------------MENU BAR------------------------------------
